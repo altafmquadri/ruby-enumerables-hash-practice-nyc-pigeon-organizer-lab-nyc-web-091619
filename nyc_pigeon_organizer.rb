3216da07eaf934616e4a62d1e90877f2ca93ef34
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-07 19:44:26 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,21 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
+
   pigeon_list = {}
   data.each do |color_type, colors|
     colors.each do |color, list|
       list.each do |name|
-        pigeon_list[name] = {color: [], gender: [], lives: []}
-          
-        binding.pry
-      
+        if !pigeon_list[name] 
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][color_type]
+          pigeon_list[name][color_type] = []
+        end
+        pigeon_list[name][color_type].push(color.to_s)
       end
     end
+    
   end
+  pigeon_list
 end
